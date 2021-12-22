1da6a0fd43ec187228c83b3406fa66856ce530eb
ale8k <alexanderkilroy@outlook.com>
2021-12-22 18:41:32 +0000

    Basic apollo server
    - So we just define gql strings (schema), and then in the resolver return said type.
    - I guess once you learn schema then it's just creating correct resolvers

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,73 @@
-console.log("yolo man");
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+import { DocumentNode } from 'graphql';
 
-function n() { console.log("hi") } 
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+
+// query GetBooks {
+//     books {
+//         title
+//         author
+//     }
+// }
+
+// curl --request POST \
+//   --header 'content-type: application/json' \
+//   --url http://localhost:4000/graphql \
+//   --data '{"query":"query { __typename }"}'
+
+async function startApolloServer(typeDefs: DocumentNode, resolvers: any) {
+    const app = express();
+    const httpServer = http.createServer(app);
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+
+    await server.start();
+    server.applyMiddleware({ app });
+    await new Promise((res) => {
+        httpServer.listen({ port: 4000 })
+        res(0)
+    });
+
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers)
